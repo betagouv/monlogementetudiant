6c0591afc0b2497000591ec29a4f1dec63aa0da4
Kevin GALLET <kevingallet@gmail.com>
2025-07-21 11:51:15 +0200

    fix: do not display current accommodation in nearby ones

--- src/components/accommodation/nearby-accommodations.tsx
+++ src/components/accommodation/nearby-accommodations.tsx
@@ -7,13 +7,17 @@ import Card, { CardProps } from '@codegouvfr/react-dsfr/Card'
 import { useTranslations } from 'next-intl'
 import { useState } from 'react'
 import { tss } from 'tss-react'
+import { TAccomodationDetails } from '~/schemas/accommodations/accommodations'
 import { TGetAccomodationsResponse } from '~/schemas/accommodations/get-accommodations'
 
-export const NearbyAccommodations = ({ nearbyAccommodations }: { nearbyAccommodations: TGetAccomodationsResponse }) => {
+export const NearbyAccommodations = ({
+  nearbyAccommodations,
+  accommodation,
+}: { nearbyAccommodations: TGetAccomodationsResponse; accommodation: TAccomodationDetails }) => {
   const { classes } = useStyles()
   const t = useTranslations('accomodation')
   const [currentIndex, setCurrentIndex] = useState(0)
-  const nearbyFeatures = nearbyAccommodations.results.features
+  const nearbyFeatures = nearbyAccommodations.results.features.filter((feature) => feature.id !== accommodation.id)
   const maxIndex = nearbyFeatures.length - 1
   const handlePrevious = () => setCurrentIndex((prev) => (prev > 0 ? prev - 1 : maxIndex))
   const handleNext = () => setCurrentIndex((prev) => (prev < maxIndex ? prev + 1 : 0))
