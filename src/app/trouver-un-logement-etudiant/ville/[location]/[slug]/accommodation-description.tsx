181a0694c88bbfe09d80bed501e854d49bccc256
Kevin GALLET <kevingallet@gmail.com>
2025-09-01 09:43:47 +0200

    feat: add residence description

---
+++ src/app/trouver-un-logement-etudiant/ville/[location]/[slug]/accommodation-description.tsx
@@ -0,0 +1,15 @@
+import { getTranslations } from 'next-intl/server'
+import styles from './logement.module.css'
+
+export default async function AccommodationDescription({ title, description }: { title: string; description: string | null }) {
+  const t = await getTranslations('accomodation')
+
+  if (!description) return null
+
+  return (
+    <div className={styles.section}>
+      <h4>{t('descriptionTitle', { title })}</h4>
+      <div dangerouslySetInnerHTML={{ __html: description }} />
+    </div>
+  )
+}
