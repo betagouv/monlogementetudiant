c553548a67e8d8bd464da77716baa396590031b2
Kevin GALLET <kevingallet@gmail.com>
2025-10-13 10:52:46 +0200

    chore: change brand top

--- src/components/ui/brand-top.tsx
+++ src/components/ui/brand-top.tsx
@@ -2,13 +2,6 @@ import { FC } from 'react'
 
 export const BrandTop: FC = () => (
   <>
-    Ministère
-    <br />
-    Chargé
-    <br />
-    de l&apos;enseignement
-    <br /> supérieur
-    <br />
-    et de la recherche
+    République <br /> Française
   </>
 )
