a7e29e5f7482b272f9c460ba7605bb63ac35c327
Kevin GALLET <kevingallet@gmail.com>
2025-12-08 18:44:44 +0100

    fix: validatoin form

--- src/schemas/accommodations/update-residence-list.ts
+++ src/schemas/accommodations/update-residence-list.ts
@@ -1,11 +1,11 @@
 import { z } from 'zod'
 
 export const ZUpdateResidenceList = z.object({
-  nb_t1_available: z.number().min(0).nullable(),
-  nb_t1_bis_available: z.number().min(0).nullable(),
-  nb_t2_available: z.number().min(0).nullable(),
-  nb_t3_available: z.number().min(0).nullable(),
-  nb_t4_more_available: z.number().min(0).nullable(),
+  nb_t1_available: z.number({ message: 'Le champs est requis' }).min(0).nullable(),
+  nb_t1_bis_available: z.number({ message: 'Le champs est requis' }).min(0).nullable(),
+  nb_t2_available: z.number({ message: 'Le champs est requis' }).min(0).nullable(),
+  nb_t3_available: z.number({ message: 'Le champs est requis' }).min(0).nullable(),
+  nb_t4_more_available: z.number({ message: 'Le champs est requis' }).min(0).nullable(),
 })
 
 export type TUpdateResidenceList = z.infer<typeof ZUpdateResidenceList>
