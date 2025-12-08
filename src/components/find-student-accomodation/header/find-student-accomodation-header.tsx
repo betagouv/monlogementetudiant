df64a884f10f83b19bcfba3466ae80e20487df93
Kevin GALLET <kevingallet@gmail.com>
2025-12-08 10:59:45 +0100

    feat: adding crous filter

--- src/components/find-student-accomodation/header/find-student-accomodation-header.tsx
+++ src/components/find-student-accomodation/header/find-student-accomodation-header.tsx
@@ -2,6 +2,7 @@ import { fr } from '@codegouvfr/react-dsfr'
 import { FC } from 'react'
 import { FindStudentAccomodationAutocompleteInput } from '~/components/find-student-accomodation/autocomplete/find-student-accomodation-autocomplete-input'
 import { FindStudentAccessibleAccomodationSwitch } from '~/components/find-student-accomodation/header/find-student-accessible-accomodation-switch'
+import { FindStudentAccommodationCrousFilter } from '~/components/find-student-accomodation/header/find-student-accommodation-crous-filter'
 import { FindStudentAccommodationPrice } from '~/components/find-student-accomodation/header/find-student-accommodation-price'
 import { FindStudentColivingAccomodationSwitch } from '~/components/find-student-accomodation/header/find-student-coliving-accomodation'
 import styles from './find-student-accomodation-header.module.css'
@@ -13,19 +14,14 @@ export const FindStudentAccomodationHeader: FC = async () => {
         <div className={styles.container}>
           <FindStudentAccomodationAutocompleteInput />
           <FindStudentAccommodationPrice />
+          <FindStudentAccommodationCrousFilter />
           <FindStudentColivingAccomodationSwitch />
           <FindStudentAccessibleAccomodationSwitch />
-          {/* Remove since we do not use it for now */}
-          {/* <Button priority="secondary" iconId="ri-equalizer-line">
-            {t('header.filtersCta')}
-          </Button> */}
         </div>
       </div>
       <div className={fr.cx('fr-hidden-sm')}>
         <div className={styles.mobileContainer}>
           <FindStudentAccomodationAutocompleteInput />
-          {/* Remove since we do not use it for now */}
-          {/* <Button priority="secondary" iconId="ri-equalizer-line" title={t('header.filtersCta')} /> */}
         </div>
       </div>
     </>
