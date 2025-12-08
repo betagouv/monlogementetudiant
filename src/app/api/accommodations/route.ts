ee0e2f297d37f2ba44424320a0525accf5cbddb5
Kevin GALLET <kevingallet@gmail.com>
2025-12-08 10:59:45 +0100

    feat: adding crous filter

--- src/app/api/accommodations/route.ts
+++ src/app/api/accommodations/route.ts
@@ -8,12 +8,14 @@ export async function GET(request: Request) {
   const bbox = url.searchParams.get('bbox')
   const page = url.searchParams.get('page')
   const maxPrice = url.searchParams.get('price_max')
+  const crous = url.searchParams.get('view_crous')
 
   if (bbox) params.append('bbox', bbox)
   if (page) params.append('page', page)
   if (maxPrice) params.append('price_max', maxPrice)
   params.append('is_accessible', isAccessible === 'true' ? 'true' : 'false')
   params.append('has_coliving', hasColiving === 'true' ? 'true' : 'false')
+  params.append('view_crous', crous === 'true' ? 'true' : 'false')
 
   const response = await fetch(`${process.env.API_URL}/accommodations/${params.size > 0 ? `?${params.toString()}` : ''}`)
   if (!response.ok) {
