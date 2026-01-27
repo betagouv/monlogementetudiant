55a6b6b6963fbd2cb842e186eda7e9d341be7f40
Kevin GALLET <kevingallet@gmail.com>
2026-01-27 10:11:57 +0100

    fix: session provider refetch

---
+++ src/providers/next-auth.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { type Session } from 'next-auth'
+import { SessionProvider } from 'next-auth/react'
+import React from 'react'
+
+export type NextAuthProviderProps = {
+  children: React.ReactNode
+  session?: Session | null
+}
+
+export const NextAuthProvider = ({ children, session }: NextAuthProviderProps) => {
+  return (
+    <SessionProvider session={session} refetchInterval={60} refetchOnWindowFocus={true}>
+      {children}
+    </SessionProvider>
+  )
+}
