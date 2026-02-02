c9b0056a4f22a65411b8442b42c2fd353883bbac
Kevin GALLET <kevingallet@gmail.com>
2026-02-02 13:32:27 +0100

    feat: help simulator cta

--- src/components/helps-simulator/help-simulator.tsx
+++ src/components/helps-simulator/help-simulator.tsx
@@ -1,8 +1,13 @@
 'use client'
 
+import Button from '@codegouvfr/react-dsfr/Button'
+import clsx from 'clsx'
+import { useTranslations } from 'next-intl'
 import { FC, useEffect, useRef } from 'react'
 import { HelpSimulatorForm } from '~/components/helps-simulator/help-simulator-form'
 import { HelpSimulatorHeader } from '~/components/helps-simulator/help-simulator-header'
+import { useHelpSimulatorStep } from '~/components/helps-simulator/use-help-simulator-step'
+import styles from './help-simulator-cta.module.css'
 
 interface HelpSimulatorProps {
   onHeightChange?: (height: number) => void
@@ -10,6 +15,8 @@ interface HelpSimulatorProps {
 
 export const HelpSimulator: FC<HelpSimulatorProps> = ({ onHeightChange }) => {
   const containerRef = useRef<HTMLDivElement>(null)
+  const [currentStep] = useHelpSimulatorStep()
+  const t = useTranslations('simulator.cta')
 
   useEffect(() => {
     if (!onHeightChange || !containerRef.current) return
@@ -31,11 +38,26 @@ export const HelpSimulator: FC<HelpSimulatorProps> = ({ onHeightChange }) => {
   }, [onHeightChange])
 
   return (
-    <div ref={containerRef}>
-      <HelpSimulatorHeader />
-      <div className="fr-py-5w fr-px-8w">
-        <HelpSimulatorForm />
+    <>
+      <div ref={containerRef}>
+        <HelpSimulatorHeader />
+        <div className="fr-py-5w fr-px-8w">
+          <HelpSimulatorForm />
+        </div>
       </div>
-    </div>
+      {currentStep === 4 && (
+        <div className={styles.container}>
+          <h2 className={clsx('fr-h3 fr-mb-2w', styles.title)}>
+            {t('titleLine1')}
+            <br />
+            {t('titleLine2')}
+          </h2>
+          <p className={clsx('fr-text--lg fr-mb-3w', styles.subtitle)}>{t('subtitle')}</p>
+          <Button linkProps={{ href: '/trouver-un-logement-etudiant' }} iconId="ri-arrow-right-line" iconPosition="right">
+            {t('button')}
+          </Button>
+        </div>
+      )}
+    </>
   )
 }
