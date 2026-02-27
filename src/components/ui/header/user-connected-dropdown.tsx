76ff536808073bf741773636d3ae15914d008db3
Kevin GALLET <kevingallet@gmail.com>
2026-02-27 09:29:33 +0100

    fix: mobile sign in button

--- src/components/ui/header/user-connected-dropdown.tsx
+++ src/components/ui/header/user-connected-dropdown.tsx
@@ -20,21 +20,33 @@ export const UserConnectedDropdown = ({ user }: { user: TUser }) => {
 
   const workspaceUrl = user.role === 'user' ? '/mon-espace' : '/bailleur/tableau-de-bord'
   return (
-    <Dropdown id="header_user_menu" alignRight control={user.name} dropdownControlClassName="fr-mb-0">
-      <ul>
-        <li className="fr-border-top fr-my-md-0 fr-my-1w fr-py-md-0 fr-py-1w fr-px-md-0 fr-px-2w">
-          <Button priority="tertiary no outline" className="fr-text--sm" linkProps={{ href: workspaceUrl, target: '_self' }}>
-            <span className="ri-account-circle-line fr-icon--sm fr-mr-1w fr-text-label--blue-france" />
-            <span className="fr-text-mention--black fr-text--normal">Revenir à mon espace</span>
-          </Button>
-        </li>
-        <li className="fr-border-top fr-border-bottom fr-my-md-0 fr-my-1w fr-py-md-0 fr-py-1w fr-px-md-0 fr-px-2w">
-          <Button priority="tertiary no outline" className="fr-text--sm" onClick={handleSignout}>
-            <span className="fr-icon-logout-box-r-line fr-icon--sm fr-mr-1w fr-text-label--blue-france" />
-            <span className="fr-text-mention--black fr-text--normal">Se déconnecter</span>
-          </Button>
-        </li>
-      </ul>
-    </Dropdown>
+    <>
+      <div className="fr-hidden-sm">
+        <Button priority="tertiary no outline" iconId="ri-account-circle-line" linkProps={{ href: workspaceUrl, target: '_self' }}>
+          Mon espace
+        </Button>
+        <Button priority="tertiary no outline" iconId="fr-icon-logout-box-r-line" onClick={handleSignout}>
+          Déconnexion
+        </Button>
+      </div>
+      <div className="fr-hidden fr-unhidden-sm">
+        <Dropdown id="header_user_menu" alignRight control={user.name} dropdownControlClassName="fr-mb-0">
+          <ul>
+            <li className="fr-border-top fr-my-md-0 fr-my-1w fr-py-md-0 fr-py-1w fr-px-md-0 fr-px-2w">
+              <Button priority="tertiary no outline" className="fr-text--sm" linkProps={{ href: workspaceUrl, target: '_self' }}>
+                <span className="ri-account-circle-line fr-icon--sm fr-mr-1w fr-text-label--blue-france" />
+                <span className="fr-text-mention--black fr-text--normal">Revenir à mon espace</span>
+              </Button>
+            </li>
+            <li className="fr-border-top fr-border-bottom fr-my-md-0 fr-my-1w fr-py-md-0 fr-py-1w fr-px-md-0 fr-px-2w">
+              <Button priority="tertiary no outline" className="fr-text--sm" onClick={handleSignout}>
+                <span className="fr-icon-logout-box-r-line fr-icon--sm fr-mr-1w fr-text-label--blue-france" />
+                <span className="fr-text-mention--black fr-text--normal">Se déconnecter</span>
+              </Button>
+            </li>
+          </ul>
+        </Dropdown>
+      </div>
+    </>
   )
 }
--- src/components/ui/header/user-sign-in-dropdown.tsx
+++ src/components/ui/header/user-sign-in-dropdown.tsx
@@ -5,25 +5,41 @@ import { Dropdown } from '~/components/ui/dropdown'
 
 export const UserSignInDropdown = () => {
   return (
-    <Dropdown id="header_sign_in_user_menu" alignRight control="Se connecter" dropdownControlClassName="fr-mb-0">
-      <ul>
-        <li className="fr-border-top fr-my-md-0 fr-my-1w fr-py-md-0 fr-py-1w fr-px-md-0 fr-px-2w">
-          <Button priority="tertiary no outline" className="fr-text--sm" linkProps={{ href: '/se-connecter', target: '_self' }}>
-            <span className="ri-account-circle-line fr-icon--sm fr-mr-1w fr-text-label--blue-france" />
-            <span className="fr-text-mention--black fr-text--normal">Espace Étudiant</span>
-          </Button>
-        </li>
-        <li className="fr-border-top fr-border-bottom fr-my-md-0 fr-my-1w fr-py-md-0 fr-py-1w fr-px-md-0 fr-px-2w">
-          <Button
-            priority="tertiary no outline"
-            className="fr-text--sm"
-            linkProps={{ href: '/gestionnaire/se-connecter', target: '_self' }}
-          >
-            <span className="ri-building-line fr-icon--sm fr-mr-1w fr-text-label--blue-france" />
-            <span className="fr-text-mention--black fr-text--normal">Espace Gestionnaire</span>
-          </Button>
-        </li>
-      </ul>
-    </Dropdown>
+    <>
+      <div className="fr-hidden-sm">
+        <Button priority="tertiary no outline" iconId="ri-account-circle-line" linkProps={{ href: '/se-connecter', target: '_self' }}>
+          Espace Étudiant
+        </Button>
+        <Button
+          priority="tertiary no outline"
+          iconId="ri-building-line"
+          linkProps={{ href: '/gestionnaire/se-connecter', target: '_self' }}
+        >
+          Espace Gestionnaire
+        </Button>
+      </div>
+      <div className="fr-hidden fr-unhidden-sm">
+        <Dropdown id="header_sign_in_user_menu" alignRight control="Se connecter" dropdownControlClassName="fr-mb-0">
+          <ul>
+            <li className="fr-border-top fr-my-md-0 fr-my-1w fr-py-md-0 fr-py-1w fr-px-md-0 fr-px-2w">
+              <Button priority="tertiary no outline" className="fr-text--sm" linkProps={{ href: '/se-connecter', target: '_self' }}>
+                <span className="ri-account-circle-line fr-icon--sm fr-mr-1w fr-text-label--blue-france" />
+                <span className="fr-text-mention--black fr-text--normal">Espace Étudiant</span>
+              </Button>
+            </li>
+            <li className="fr-border-top fr-border-bottom fr-my-md-0 fr-my-1w fr-py-md-0 fr-py-1w fr-px-md-0 fr-px-2w">
+              <Button
+                priority="tertiary no outline"
+                className="fr-text--sm"
+                linkProps={{ href: '/gestionnaire/se-connecter', target: '_self' }}
+              >
+                <span className="ri-building-line fr-icon--sm fr-mr-1w fr-text-label--blue-france" />
+                <span className="fr-text-mention--black fr-text--normal">Espace Gestionnaire</span>
+              </Button>
+            </li>
+          </ul>
+        </Dropdown>
+      </div>
+    </>
   )
 }
