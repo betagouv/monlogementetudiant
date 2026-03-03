89a55d19d0effd226746e0c4cf0cc5fc4baf2340
Kevin GALLET <kevingallet@gmail.com>
2026-03-03 14:24:55 +0100

    chore: slugs + label residence type

--- src/schemas/accommodations/update-residence.ts
+++ src/schemas/accommodations/update-residence.ts
@@ -1,27 +1,28 @@
 import { z } from 'zod'
 
 export enum EResidenceType {
-  UNIVERSITAIRE_CONVENTIONNEE = 'universitaire-conventionnee',
-  SOCIALE_JEUNES_ACTIFS = 'sociale-jeunes-actifs',
-  INTERGENERATIONNELLE = 'intergenerationnelle',
-  JEUNES_TRAVAILLEURS = 'jeunes-travailleurs',
-  SOCIAL_FLECHE_JEUNE = 'social-fleche-jeune',
-  SOCIAL_CLASSIQUE = 'social-classique',
-  ECOLE = 'ecole',
+  UNIVERSITAIRE_CONVENTIONNEE = 'residence-universitaire-conventionnee',
   RESIDENCE_ETUDIANTE = 'residence-etudiante',
+  SOCIALE_JEUNES_ACTIFS = 'residence-sociale-jeunes-actifs',
+  JEUNES_TRAVAILLEURS = 'foyer-jeunes-travailleurs',
+  SOCIAL_CLASSIQUE = 'logement-social-classique',
+  SOCIAL_FLECHE_JEUNE = 'loi-elan-article-109',
+  ECOLE = 'residence-ecole',
   SOUS_LOCATION = 'sous-location',
+  INTERGENERATIONNELLE = 'intergenerationnel',
 }
 
 export const RESIDENCE_TYPE_LABELS: Record<EResidenceType, string> = {
-  [EResidenceType.UNIVERSITAIRE_CONVENTIONNEE]: 'Résidence Universitaire conventionnée',
-  [EResidenceType.SOCIALE_JEUNES_ACTIFS]: 'Résidence sociale Jeunes Actifs (RSJA, Habitat Jeunes)',
-  [EResidenceType.INTERGENERATIONNELLE]: 'Cohabitation intergénérationnelle',
-  [EResidenceType.JEUNES_TRAVAILLEURS]: 'Foyer Jeunes Travailleurs (FJT)',
-  [EResidenceType.SOCIAL_FLECHE_JEUNE]: 'Logement social fléché vers les jeunes (loi ELAN - article 109)',
-  [EResidenceType.SOCIAL_CLASSIQUE]: 'Logement social classique commercialisé en partie aux étudiants',
-  [EResidenceType.ECOLE]: "Résidence d'école ou d'établissement d'enseignement",
+  [EResidenceType.UNIVERSITAIRE_CONVENTIONNEE]: 'Résidence universitaire conventionnée',
   [EResidenceType.RESIDENCE_ETUDIANTE]: 'Résidence étudiante',
-  [EResidenceType.SOUS_LOCATION]: 'Logements sociaux sous-loués aux étudiants par une association',
+  [EResidenceType.SOCIALE_JEUNES_ACTIFS]: 'Résidence Sociale Jeunes Actifs (RSJA, Habitat Jeunes)',
+  [EResidenceType.JEUNES_TRAVAILLEURS]: 'Foyer de jeunes travailleurs (FJT)',
+  [EResidenceType.SOCIAL_CLASSIQUE]: 'Logement social classique commercialisé en partie aux étudiants',
+  [EResidenceType.SOCIAL_FLECHE_JEUNE]: 'Logement social fléché vers les jeunes (loi ELAN – article 109)',
+  [EResidenceType.ECOLE]: "Résidence d'école ou d'établissement d'enseignement",
+  [EResidenceType.SOUS_LOCATION]:
+    'Logements sociaux sous-loués aux étudiants par une association via une convention ou un contrat spécifique',
+  [EResidenceType.INTERGENERATIONNELLE]: 'Cohabitation intergénérationnelle',
 }
 
 export enum ETargetAudience {
