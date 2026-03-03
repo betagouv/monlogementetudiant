fb73710484e9734c50e0ba13bc673c372aeff3f9
Kevin GALLET <kevingallet@gmail.com>
2026-03-03 17:59:08 +0100

    fix: build

--- src/server-only/get-departments.ts
+++
@@ -1,6 +0,0 @@
-import { TDepartments } from '~/schemas/departments'
-import { getQueryClient, trpc } from '~/server/trpc/server'
-
-export const getDepartments = async (): Promise<TDepartments> => {
-  return getQueryClient().fetchQuery(trpc.territories.listDepartments.queryOptions())
-}
