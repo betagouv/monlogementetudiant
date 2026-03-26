b0eb94ce31cef9ae155aa1b1c30fcac24a5f8c21
Kevin GALLET <kevingallet@gmail.com>
2026-03-26 15:07:27 +0100

    chore: rebase

--- drizzle/0009_bored_talon.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "dossier_facile_application" ADD COLUMN "status" text DEFAULT 'pending' NOT NULL;--> statement-breakpoint
-ALTER TABLE "dossier_facile_application" ADD COLUMN "reviewed_at" timestamp with time zone;
