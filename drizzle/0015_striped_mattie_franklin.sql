35775dec21c949fc27fa08121d285f49a65853f8
Ebrard Alexis <dakie62@Mac-mini-de-Ebrard.local>
2026-03-30 10:04:41 +0200

    chore: add new migrations

---
+++ drizzle/0015_striped_mattie_franklin.sql
@@ -0,0 +1,16 @@
+ALTER TABLE "accommodation_accommodation" ADD COLUMN "superficie_min_t1" integer;--> statement-breakpoint
+ALTER TABLE "accommodation_accommodation" ADD COLUMN "superficie_max_t1" integer;--> statement-breakpoint
+ALTER TABLE "accommodation_accommodation" ADD COLUMN "superficie_min_t1_bis" integer;--> statement-breakpoint
+ALTER TABLE "accommodation_accommodation" ADD COLUMN "superficie_max_t1_bis" integer;--> statement-breakpoint
+ALTER TABLE "accommodation_accommodation" ADD COLUMN "superficie_min_t2" integer;--> statement-breakpoint
+ALTER TABLE "accommodation_accommodation" ADD COLUMN "superficie_max_t2" integer;--> statement-breakpoint
+ALTER TABLE "accommodation_accommodation" ADD COLUMN "superficie_min_t3" integer;--> statement-breakpoint
+ALTER TABLE "accommodation_accommodation" ADD COLUMN "superficie_max_t3" integer;--> statement-breakpoint
+ALTER TABLE "accommodation_accommodation" ADD COLUMN "superficie_min_t4" integer;--> statement-breakpoint
+ALTER TABLE "accommodation_accommodation" ADD COLUMN "superficie_max_t4" integer;--> statement-breakpoint
+ALTER TABLE "accommodation_accommodation" ADD COLUMN "superficie_min_t5" integer;--> statement-breakpoint
+ALTER TABLE "accommodation_accommodation" ADD COLUMN "superficie_max_t5" integer;--> statement-breakpoint
+ALTER TABLE "accommodation_accommodation" ADD COLUMN "superficie_min_t6" integer;--> statement-breakpoint
+ALTER TABLE "accommodation_accommodation" ADD COLUMN "superficie_max_t6" integer;--> statement-breakpoint
+ALTER TABLE "accommodation_accommodation" ADD COLUMN "superficie_min_t7_more" integer;--> statement-breakpoint
+ALTER TABLE "accommodation_accommodation" ADD COLUMN "superficie_max_t7_more" integer;
