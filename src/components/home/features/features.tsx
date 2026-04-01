d3465d9d8ec3a08913383cd3c0c65533858c12ce
Ebrard Alexis <dakie62@Mac-mini-de-Ebrard.local>
2026-04-01 10:27:47 +0200

    Card features

--- src/components/home/features/features.tsx
+++ src/components/home/features/features.tsx
@@ -2,8 +2,8 @@ import { fr } from '@codegouvfr/react-dsfr'
 import Badge from '@codegouvfr/react-dsfr/Badge'
 import Button from '@codegouvfr/react-dsfr/Button'
 import clsx from 'clsx'
-import Image from 'next/image'
 import { getTranslations } from 'next-intl/server'
+import Image from 'next/image'
 import styles from './features.module.css'
 
 export const FeaturesSection = async () => {
@@ -16,18 +16,18 @@ export const FeaturesSection = async () => {
           <Badge severity="new" noIcon className={styles.badgePurple}>
             {tHome('features.simulateAids.badge')}
           </Badge>
-          <h2 className={clsx('fr-h3', styles.featureCardTitle)}>{tHome('features.simulateAids.title')}</h2>
-          <p className={clsx(fr.cx('fr-text--lg'), styles.featureCardDescription)}>{tHome('features.simulateAids.description')}</p>
+          <h2 className={clsx('fr-h2', styles.featureCardTitle)}>{tHome('features.simulateAids.title')}</h2>
+          <p className={clsx(fr.cx('fr-text--xl'), styles.featureCardDescription)}>{tHome('features.simulateAids.description')}</p>
           <div className={styles.featureCardButtons}>
             <Button priority="secondary" iconId="ri-money-euro-circle-line" iconPosition="left" linkProps={{ href: '/simuler-mes-aides' }}>
               {tHome('features.simulateAids.button')}
             </Button>
             <div className={styles.featureCardLogos}>
-              <Image src="/images/apl.svg" width={60} height={30} alt="APL" />
-              <Image src="/images/logo-crous.svg" width={60} height={30} alt="CROUS" />
-              <Image src="/images/caf.svg" width={60} height={30} alt="CAF" />
-              <Image src="/images/al.svg" width={60} height={30} alt="Action Logement" />
-              <Image src="/images/mobilijeune.svg" width={60} height={30} alt="Mobilijeune" />
+              <Image src="/images/apl.svg" width={23} height={30} alt="APL" />
+              <Image src="/images/logo-crous.svg" width={36} height={36} alt="CROUS" />
+              <Image src="/images/caf.svg" width={31} height={30} alt="CAF" />
+              <Image src="/images/al.svg" width={31} height={32} alt="Action Logement" />
+              <Image src="/images/mobilijeune.svg" width={49} height={36} alt="Mobilijeune" />
             </div>
           </div>
         </div>
@@ -36,16 +36,18 @@ export const FeaturesSection = async () => {
           <Badge severity="info" noIcon className={styles.badgeYellow}>
             {tHome('features.calculateBudget.badge')}
           </Badge>
-          <h2 className={clsx('fr-h3', styles.featureCardTitle)}>{tHome('features.calculateBudget.title')}</h2>
-          <p className={clsx(fr.cx('fr-text--lg'), styles.featureCardDescription)}>{tHome('features.calculateBudget.description')}</p>
-          <Button
-            priority="secondary"
-            iconId="ri-calculator-line"
-            iconPosition="left"
-            linkProps={{ href: '/preparer-mon-budget-etudiant' }}
-          >
-            {tHome('features.calculateBudget.button')}
-          </Button>
+          <h2 className={clsx('fr-h2', styles.featureCardTitle)}>{tHome('features.calculateBudget.title')}</h2>
+          <p className={clsx(fr.cx('fr-text--xl'), styles.featureCardDescription)}>{tHome('features.calculateBudget.description')}</p>
+          <div className={styles.featureCardButtons}>
+            <Button
+              priority="secondary"
+              iconId="ri-calculator-line"
+              iconPosition="left"
+              linkProps={{ href: '/preparer-mon-budget-etudiant' }}
+            >
+              {tHome('features.calculateBudget.button')}
+            </Button>
+          </div>
         </div>
       </div>
     </section>
