0d875770b65c8adb74f302440627c0b23fa9f305
Ebrard Alexis <dakie62@Mac-mini-de-Ebrard.local>
2026-04-02 10:43:47 +0200

    facilitate section

--- src/components/home/facilitate/facilitate.tsx
+++ src/components/home/facilitate/facilitate.tsx
@@ -1,6 +1,6 @@
-import { fr } from '@codegouvfr/react-dsfr'
 import Button from '@codegouvfr/react-dsfr/Button'
 import clsx from 'clsx'
+import Image from 'next/image'
 import { getTranslations } from 'next-intl/server'
 import styles from './facilitate.module.css'
 
@@ -8,31 +8,86 @@ export const FacilitateSection = async () => {
   const tHome = await getTranslations('home')
   return (
     <section className={styles.facilitateSection}>
-      <div className={fr.cx('fr-container')}>
+      <div>
         <div className={styles.facilitateSectionContent}>
-          <div className={styles.facilitateIllustration} />
+          <div className={styles.faciliteSectionContentLeft}>
+            <div>
+              <h2 className={clsx('fr-h1', styles.facilitateTitle)}>{tHome('facilitate.title')}</h2>
+              <h2 className={clsx('fr-h1', styles.highlight)}>{tHome('facilitate.titleHighlight')}</h2>
+            </div>
+            <Image
+              className={styles.facilitateIllustration}
+              src="/images/facilities.svg"
+              alt={tHome('hero.illustrationAlt')}
+              width={811}
+              height={500}
+            />
+          </div>
           <div className={styles.facilitateContent}>
-            <h2 className={clsx('fr-h2', styles.facilitateTitle)}>{tHome('facilitate.title')}</h2>
             <ul className={styles.facilitateList}>
               <li className={styles.facilitateItem}>
-                <span className={fr.cx('ri-home-line', 'fr-mr-2w')} aria-hidden="true" />
-                {tHome('facilitate.items.offers')}
+                <div className={styles.facilitateItemBefore}>
+                  <div>
+                    <i className={clsx('ri-hotel-line', styles.facilitateIcon)} aria-hidden="true" />
+                  </div>
+                  <div></div>
+                </div>
+                <div>
+                  <span>{tHome('facilitate.items.offers')}</span>
+                  <br />
+                  <span>{tHome('facilitate.items.offersDescription')}</span>
+                </div>
               </li>
               <li className={styles.facilitateItem}>
-                <span className={fr.cx('ri-calculator-line', 'fr-mr-2w')} aria-hidden="true" />
-                {tHome('facilitate.items.simulator')}
+                <div className={styles.facilitateItemBefore}>
+                  <div>
+                    <i className={clsx('ri-money-euro-circle-line', styles.facilitateIcon)} aria-hidden="true" />
+                  </div>
+                  <div></div>
+                </div>
+                <div>
+                  <span>{tHome('facilitate.items.simulator')}</span>
+                  <br />
+                  <span>{tHome('facilitate.items.simulatorDescription')}</span>
+                </div>
               </li>
               <li className={styles.facilitateItem}>
-                <span className={fr.cx('ri-money-euro-circle-line', 'fr-mr-2w')} aria-hidden="true" />
-                {tHome('facilitate.items.calculator')}
+                <div className={styles.facilitateItemBefore}>
+                  <div>
+                    <i className={clsx('ri-calculator-line', styles.facilitateIcon)} aria-hidden="true" />
+                  </div>
+                  <div></div>
+                </div>
+                <div>
+                  <span>{tHome('facilitate.items.calculator')}</span>
+                  <br />
+                  <span>{tHome('facilitate.items.calculatorDescription')}</span>
+                </div>
               </li>
               <li className={styles.facilitateItem}>
-                <span className={fr.cx('ri-notification-line', 'fr-mr-2w')} aria-hidden="true" />
-                {tHome('facilitate.items.alerts')}
+                <div className={styles.facilitateItemBefore}>
+                  <div>
+                    <i className={clsx('ri-mail-unread-line', styles.facilitateIcon)} aria-hidden="true" />
+                  </div>
+                  <div></div>
+                </div>
+                <div>
+                  <span>{tHome('facilitate.items.alerts')}</span>
+                  <br />
+                  <span>{tHome('facilitate.items.alertsDescription')}</span>
+                </div>
               </li>
               <li className={styles.facilitateItem}>
-                <span className={fr.cx('ri-user-line', 'fr-mr-2w')} aria-hidden="true" />
-                {tHome('facilitate.items.account')}
+                <div className={styles.facilitateItemBefore}>
+                  <div>
+                    <i className={clsx('ri-user-line', styles.facilitateIcon)} aria-hidden="true" />
+                  </div>
+                </div>
+                <div>
+                  <span>{tHome('facilitate.items.account')}</span>
+                  <br />
+                  <span>{tHome('facilitate.items.accountDescription')}</span>
+                </div>
               </li>
             </ul>
             <Button className={styles.facilitateButton} linkProps={{ href: '/inscription' }}>
