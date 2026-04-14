7c96dca0a7c6bfe7e766ae1719eb14c70bec6a3a
Kevin GALLET <kevingallet@gmail.com>
2026-04-14 09:29:38 +0200

    fix: facilitate image + big search bar

--- src/components/home/hero-section/hero-search-bar.tsx
+++ src/components/home/hero-section/hero-search-bar.tsx
@@ -1,7 +1,6 @@
 'use client'
 
-import Button from '@codegouvfr/react-dsfr/Button'
-import Input from '@codegouvfr/react-dsfr/Input'
+import { SearchBar } from '@codegouvfr/react-dsfr/SearchBar'
 import { useRouter } from 'next/navigation'
 import { FC, useMemo } from 'react'
 import { FindStudentAccomodationAutocompleteResults } from '~/components/find-student-accomodation/autocomplete/find-student-accomodation-autocomplete-results'
@@ -10,7 +9,7 @@ import styles from './hero-search-bar.module.css'
 
 export const HeroSearchBar: FC = () => {
   const router = useRouter()
-  const { data, isError, searchQuery, setSearchQuery } = useTerritories()
+  const { data, searchQuery, setSearchQuery } = useTerritories()
 
   const searchHref = useMemo(() => {
     if (searchQuery) {
@@ -31,26 +30,25 @@ export const HeroSearchBar: FC = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.searchBar}>
-        <div className={styles.inputContainer}>
-          <Input
-            classes={{ root: styles.input, nativeInputOrTextArea: styles.nativeInput }}
-            label=""
-            hideLabel
-            nativeInputProps={{
-              placeholder: 'Ville, académie ou département',
-              onChange: handleInputChange,
-              onKeyDown: handleKeyDown,
-              value: searchQuery,
-            }}
-            state={isError ? 'error' : 'default'}
-          />
-          {data && <FindStudentAccomodationAutocompleteResults data={data} />}
-        </div>
-        <Button className={`fr-hidden fr-unhidden-sm ${styles.searchButton}`} iconId="ri-search-line" linkProps={{ href: searchHref }}>
-          Rechercher
-        </Button>
-        <Button className="fr-hidden-sm" iconId="ri-search-line" title="Rechercher" linkProps={{ href: searchHref }} />
+      <div className={styles.inputContainer}>
+        <SearchBar
+          className={styles.searchBar}
+          big
+          label="Rechercher"
+          renderInput={({ className, id, type }) => (
+            <input
+              className={`${className} ${styles.nativeInput}`}
+              id={id}
+              type={type}
+              placeholder="Ville, académie ou département"
+              value={searchQuery}
+              onChange={handleInputChange}
+              onKeyDown={handleKeyDown}
+            />
+          )}
+          onButtonClick={() => router.push(searchHref)}
+        />
+        {data && <FindStudentAccomodationAutocompleteResults data={data} />}
       </div>
     </div>
   )
