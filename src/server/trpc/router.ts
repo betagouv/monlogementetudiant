efc829e5a2359fbbe2d14055da96a2670c379e6c
Kevin GALLET <kevingallet@gmail.com>
2026-05-04 17:18:50 +0200

    feat: banner nps

--- src/server/trpc/router.ts
+++ src/server/trpc/router.ts
@@ -5,6 +5,7 @@ import { alertsRouter } from './routers/alerts'
 import { bailleurRouter } from './routers/bailleur'
 import { dossierFacileRouter } from './routers/dossier-facile'
 import { favoritesRouter } from './routers/favorites'
+import { ownerFeedbackRouter } from './routers/owner-feedback'
 import { ownerStatisticsRouter } from './routers/owner-statistics'
 import { questionsAnswersRouter } from './routers/questions-answers'
 import { territoriesRouter } from './routers/territories'
@@ -21,6 +22,7 @@ export const appRouter = createTRPCRouter({
   dossierFacile: dossierFacileRouter,
   tracking: trackingRouter,
   ownerStatistics: ownerStatisticsRouter,
+  ownerFeedback: ownerFeedbackRouter,
 })
 
 export type AppRouter = typeof appRouter
